71b5e55e0a2b44ab63a06c80ebc16ae67752d3ea
John <xxjmgamerx@gmail.com>
2026-02-05 20:56:27 +0800

    While loop with variables

--- Main.java
+++ Main.java
@@ -2,6 +2,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Hello, World!");
+        Scanner c = new Scanner(System.in);
+
+		String name;
+        String response;
+
+        int age;
+
+        double price = 2000;
+        double minor = 0.04;
+        double adult = 0;
+        double senior = 0.06;
+        double total;
+        double totalamt;
+        double totalds;
+
+        boolean buyAgain = true;
+
+        while (buyAgain) {
+
+            System.out.print("DO YOU WANT TO BUY AGAIN?(y/n): ");
+            response = c.next().toLowerCase();
+            buyAgain = response.equalsIgnoreCase("y");
+        }
+
+        c.close();
     }
 }
